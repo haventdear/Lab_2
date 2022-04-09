2c62bf9f3d30d72fd33de4ee09c4da7551a54e08
Сергей Степаненко <99740487+haventdear@users.noreply.github.com>
2022-04-09 09:06:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (args.Length == 0)
+					throw new Exception("Отсутствует входной аргумент!");
+				int timeout = int.Parse(args[0]);
+				CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+				CancellationToken token = cancelTokenSource.Token;
+				Task task = Task.Run(() => Processing(token));
+				Thread.Sleep(timeout);
+				cancelTokenSource.Cancel();
+				task.Wait();
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Входной аргумент имеет неправильный формат!");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+		static void Processing(CancellationToken token)
+		{
+			Thread.Sleep(100);
+			if (token.IsCancellationRequested)
+			{
+				Console.WriteLine("Задача завершена");
+				return;
+			}
+			Console.WriteLine("Задача выполняется");
+			Thread.Sleep(100);
+			Console.WriteLine("Задача завершена");
+		}
+	}
+}
